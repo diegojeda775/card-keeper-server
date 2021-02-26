bdd33c38a94f6ff60fcbc6194d03b689d6ac190e
Diego Ojeda Rivera <diegojeda775@yahoo.com>
2021-02-26 15:19:35 -0800

    cors

--- src/app.js
+++ src/app.js
@@ -16,9 +16,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors({
-  origin: CLIENT_ORIGIN
-}))
+app.use(cors())
 
 app.use('/api/sets', setsRouter)
 app.use('/api/cards', cardsRouter)
